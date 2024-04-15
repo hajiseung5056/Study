4b11b2d91e9a4b010c4d2dff89615a859c54465d
하지승 <cjswp051@naver.com>
2024-04-15 14:11:33 +0900

    Day1

---
+++ Java/Q23_3_1.java
@@ -0,0 +1,31 @@
+package Java;
+
+public class Q23_3_1 {
+    public static void main(String[] args) {
+        A b = new B();
+        b.paint();
+        b.draw();
+    }
+}
+
+class A{
+    public void paint(){
+        System.out.print("A");
+        draw();
+    }
+    public void draw(){
+        System.out.print("B");
+        draw();
+    }
+}
+
+class B extends A{
+    public void paint(){
+        super.draw();
+        System.out.print("C");
+        this.draw();
+    }
+    public void draw(){
+        System.out.print("D");
+    }
+}
