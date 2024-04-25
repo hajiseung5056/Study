63f66c90e2c9e9a5946596fdfc948f1ac750f86b
하지승 <cjswp051@naver.com>
2024-04-25 09:48:25 +0900

    Day7_1

---
+++ Java/Q20_4_5.java
@@ -0,0 +1,15 @@
+package Java;
+
+public class Q20_4_5 {
+    public static void main(String[] args) {
+        int[]a = new int[8];
+        int i=0; int n=10;
+        while(n>0){
+            a[i++] = n%2;
+            n /=2;
+        }
+        for(i=7;i>=0;i--){
+            System.out.print(a[i]);
+        }
+    }
+}
