79f9ba5991cfedc153fab6344a91958d17c6efed
하지승 <cjswp051@naver.com>
2024-05-07 17:10:44 +0900

    Python 공부 1일차_4

---
+++ Python/BasicCode/029.py
@@ -0,0 +1,3 @@
+string = "abcdefe2a354a32a"
+string_replace = string.replace("a","A")
+print(string_replace)
---
+++ Python/BasicCode/030.py
@@ -0,0 +1,4 @@
+string = 'abcd'
+string.replace('b','B')
+print(string)
+print(string.replace('b','B'))
