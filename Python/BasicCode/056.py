28ddc8516674f62e0b4c04532ed7dcc3a33edc26
하지승 <cjswp051@naver.com>
2024-05-10 11:42:14 +0900

    Python 공부 4일차_3

---
+++ Python/BasicCode/056.py
@@ -0,0 +1,6 @@
+lang1 = ["C","C++","JAVA"]
+lang2 = ["Python","Go","C#"]
+
+langs = lang1 + lang2
+
+print(langs)
---
+++ Python/BasicCode/057.py
@@ -0,0 +1,6 @@
+nums = [1,2,3,4,5,6,7]
+
+max = max(nums)
+min = min(nums)
+
+print(max, min)
