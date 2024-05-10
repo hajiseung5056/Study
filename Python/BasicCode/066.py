1449a1274ffc5431f2513f49c261155fc3b5d8a8
하지승 <cjswp051@naver.com>
2024-05-10 14:32:08 +0900

    Python 공부 4일차_3

---
+++ Python/BasicCode/066.py
@@ -0,0 +1,4 @@
+interest = ["삼성전자","LG전자","Naver","SK하이닉스","미래에셋대우"]
+
+print(interest[0],interest[1],interest[2],interest[3],interest[4],sep=" ")
+print(" ".join(interest))
---
+++ Python/BasicCode/069.py
@@ -0,0 +1,5 @@
+string = "삼성전자/LG전자/Naver"
+
+list_string = string.split("/")
+
+print(list_string)
