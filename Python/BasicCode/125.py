7b3f1ce45e5dc904270ebb5a9da03a94c02e768e
하지승 <cjswp051@naver.com>
2024-05-20 10:15:07 +0900

    Python 공부 9일차_1

--- Python/BasicCode/125.py
+++ Python/BasicCode/125.py
@@ -1,3 +1,9 @@
-Comp = {"011":"SKT","016":"KT","019":"LGU","010":"알수없음"}
+comp = {"011":"SKT","016":"KT","019":"LGU","010":"알수없음"}
 
-Phone = input()
+phone = input("입력:")
+
+phone1 = phone[:3]
+
+comp1 = comp[phone1]
+
+print("당신은",comp1,"사용자입니다.")
