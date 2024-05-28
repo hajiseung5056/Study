62da109325da07dd3862de98eecec0d0d8110efc
하지승 <cjswp051@naver.com>
2024-05-28 17:25:03 +0900

    Python 공부 14일차_4

---
+++ Python/BasicCode/248.py
@@ -0,0 +1,5 @@
+import os
+
+a = os.getcwd()
+
+print(a,type(a))
---
+++ Python/BasicCode/249.py
@@ -0,0 +1,4 @@
+import os
+
+os.rename("C:/Study/test1.txt", "C:/Study/test2.txt")
+os.rename("C:/Users/하지승/Desktop/VScode/test.txt", "C:/Users/하지승/Desktop/VScode/test1.txt")
