ab12eb385bf1c594a9a1a03e565ce665f6c09822
하지승 <cjswp051@naver.com>
2024-09-02 14:56:11 +0900

    Python 공부 17일차_1

---
+++ Python/BasicCode/266.py
@@ -0,0 +1,30 @@
+class Stock:
+    def __init__(self,name,code,per,pbr,배당수익률):
+        self.name = name
+        self.code = code
+        self.per = per
+        self.pbr = pbr
+        self.배당수익률 = 배당수익률
+
+
+    def set_name(self,name):
+        self.name = name
+
+    def set_code(self,code):
+        self.code = code
+
+    def set_code(self,per):
+        self.per = per
+
+    def set_code(self,pbr):
+        self.pbr = pbr
+
+    def set_code(self,배당수익률):
+        self.배당수익률 = 배당수익률
+
+
+    def get_name(self):
+        return self.name
+    
+    def get_code(self):
+        return self.code
