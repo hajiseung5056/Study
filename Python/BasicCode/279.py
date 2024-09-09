932deda7ca37d4f363164faa5c1579d01378ca60
하지승 <cjswp051@naver.com>
2024-09-09 15:29:01 +0900

    Python공부 21일차_1

---
+++ Python/BasicCode/279.py
@@ -0,0 +1,60 @@
+import random
+
+class Account:
+
+    acc_cnt = 0
+
+    def __init__(self,name,money):
+        self.deposit_cnt = 0
+        self.name = name
+        self.money = money
+        self.bank = "SC은행"
+        
+        acc1 = random.randint(0,999)
+        acc2 = random.randint(0,99)
+        acc3 = random.randint(0,999999)
+
+        acc1 = str(acc1).zfill(3)
+        acc2 = str(acc2).zfill(2)
+        acc3 = str(acc3).zfill(6)
+        
+        self.accountnum = acc1 + "-" + acc2 + "-" + acc3
+
+        Account.acc_cnt += 1
+    
+    @classmethod
+    def get_account_num(cls):
+        return cls.acc_cnt
+    
+    def deposit(self,amount):
+        if amount >= 1: 
+            self.money += amount
+            
+            self.deposit_cnt += 1
+            if self.deposit_cnt % 5 == 0:
+                self.money = self.money * 1.01
+
+
+    def withdraw(self,amount):
+        if amount < self.money:
+            self.money -= amount
+
+
+    def display_info(self):
+        print("은행이름: " + self.bank)
+        print("예금주: " + self.name)
+        print("계좌번호: " + self.accountnum)
+        print("잔고: " + str(self.money) + "원")
+
+data = []
+k = Account("KIM", 10000000)
+l = Account("LEE", 10000)
+p = Account("PARK", 10000)
+
+data.append(k)
+data.append(l)
+data.append(p)
+
+for i in data:
+    if i.money > 1000000:
+        print(i.bank + " / " + i.name + " / " + i.accountnum + " / " + str(i.money))
