697cf331e3113b4509d644a61b2976e5485755c6
하지승 <cjswp051@naver.com>
2024-09-23 17:29:12 +0900

    Python공부 23일차_4

---
+++ Python/BasicCode/292.py
@@ -0,0 +1,5 @@
+f = open("C:/Users/하지승/Desktop/test/매수종목2.txt", mode='wt')
+f.write("005930 삼성전자\n")
+f.write("005380 현대차\n")
+f.write("035420 NAVER")
+f.close
---
+++ Python/BasicCode/293.py
@@ -0,0 +1,5 @@
+f = open("C:/Users/하지승/Desktop/test/매수종목.csv", mode='wt',encoding='cp949')
+f.write("종목명,종목코드,PER\n")
+f.write("삼성전자,005930,15.79\n")
+f.write("NAVER,035420,55.82\n")
+f.close
